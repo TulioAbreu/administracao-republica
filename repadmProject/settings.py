c7f4521078940b0d3c5cbc032783372632743089
TulioAbreu <marco-tulioabreu1@hotmail.com>
2019-06-29 14:13:12 -0300

    Troca de password adicionada

--- repadmProject/settings.py
+++ repadmProject/settings.py
@@ -56,8 +56,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            os.path.join(BASE_DIR, 'app/templates'),
         ],
-        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
